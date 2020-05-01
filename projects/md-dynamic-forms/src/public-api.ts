3607c02fa500b026a8c8e7a77f875151283969fe
officialMKL <mkl.98@gmx.de>
2020-05-01 20:47:26 +0200

    deleted empty service

--- projects/md-dynamic-forms/src/public-api.ts
+++ projects/md-dynamic-forms/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of md-dynamic-forms
  */
 
-export * from './lib/md-dynamic-forms.service';
 export * from './lib/md-dynamic-forms.component';
 export * from './lib/md-dynamic-forms.module';
 export * from './lib/model/field-config.interface';
