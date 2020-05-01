b41fd071098e8a405479c40f946b83cf61c87900
officialMKL <mkl.98@gmx.de>
2020-05-01 20:47:03 +0200

    added missing imports

--- projects/md-dynamic-forms/src/lib/md-dynamic-forms.component.ts
+++ projects/md-dynamic-forms/src/lib/md-dynamic-forms.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FieldConfig} from './model/field-config.interface';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {Validator} from './model/validator.interface';
 
 @Component({
   selector: 'md-dynamic-forms',
