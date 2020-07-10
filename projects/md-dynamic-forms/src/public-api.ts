ae7a6e287b316a64b201ce1b5dd5bf8c94ce24c0
officialMKL <mkl.98@gmx.de>
2020-07-10 22:43:11 +0200

    added to public api

--- projects/md-dynamic-forms/src/public-api.ts
+++ projects/md-dynamic-forms/src/public-api.ts
@@ -6,4 +6,5 @@ export * from './lib/md-dynamic-forms.component';
 export * from './lib/md-dynamic-forms.module';
 export * from './lib/model/field-config.interface';
 export * from './lib/model/validator.interface';
+export * from './lib/model/async-validator.interface';
 export * from './lib/model/component-set';
