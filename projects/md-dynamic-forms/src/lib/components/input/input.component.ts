f2e8ecdf3a52288902e846526084eb8de135cc55
officialMKL <mkl.98@gmx.de>
2020-07-14 20:10:54 +0200

    added aria-attributes to input and added tests

--- projects/md-dynamic-forms/src/lib/components/input/input.component.ts
+++ projects/md-dynamic-forms/src/lib/components/input/input.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {BaseElement} from '../base-element';
 import {FieldInput} from '../../model/form/control/field-input';
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'md-input',
   template: `
       <mat-form-field class="demo-full-width" [formGroup]="group">
-        <input matInput [formControlName]="field.name" [placeholder]="field.label" [type]="field.inputType">
+        <input matInput [formControlName]="field.name" [placeholder]="field.label" [required]="required" [attr.aria-label]="field.label" [type]="field.inputType">
         <ng-container *ngFor="let validation of field.validations;" ngProjectAs="mat-error">
           <mat-error *ngIf="group.get(field.name).hasError(validation.name)">{{validation.message}}</mat-error>
         </ng-container>
@@ -17,5 +18,9 @@ import {FieldInput} from '../../model/form/control/field-input';
 export class InputComponent extends BaseElement<FieldInput> {
   constructor() {
     super();
+  }
+
+  get required() {
+    return  this.field.validations.find((validation) => validation.validator === Validators.required) !== undefined;
   }
 }
