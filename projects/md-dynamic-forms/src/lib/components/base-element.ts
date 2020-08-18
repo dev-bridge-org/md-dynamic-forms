86f0bbb2fbff8c202d975ca176f415211f476001
officialMKL <mkl.98@gmx.de>
2020-08-18 19:57:03 +0200

    added dependencyFields

--- projects/md-dynamic-forms/src/lib/components/base-element.ts
+++ projects/md-dynamic-forms/src/lib/components/base-element.ts
@@ -1,7 +1,15 @@
-import {FormGroup} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup} from '@angular/forms';
 import {BaseField} from '../model/form/base-field';
 
 export class BaseElement<T extends BaseField> {
   field: T;
   group: FormGroup;
+
+  get control(): AbstractControl {
+    return this.group.get(this.field.name);
+  }
+
+  get dependencyControls(): AbstractControl[] {
+    return this.field.dependencies.map(dependency => this.group.root.get(dependency));
+  }
 }
