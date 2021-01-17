6e3a9a04246365f4fe40dfbd96d98c3307a9cd2f
officialMKL <mkl.98@gmx.de>
2021-01-17 20:54:03 +0100

    added errorKeys for internal file-input

--- projects/md-dynamic-forms-core/src/lib/model/form/control/field-file.ts
+++ projects/md-dynamic-forms-core/src/lib/model/form/control/field-file.ts
@@ -1,12 +1,16 @@
 import {FieldInput, FieldInputConfig} from './field-input';
 
 export interface FieldFileConfig extends FieldInputConfig {
+  errorKeys?: string[];
 }
 
 export class FieldFile extends FieldInput {
+  errorKeys: string[];
+
   constructor(config: FieldFileConfig) {
     super(config);
 
+    this.errorKeys = config.errorKeys || [];
     this.component = 'file';
     this.inputType = 'file';
   }
