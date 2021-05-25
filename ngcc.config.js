85219c39772e28b9e103d0c55686ef68d63a5190
officialMKL <mkl.98@gmx.de>
2021-05-25 17:50:28 +0200

    made some changes to the setup

--- ngcc.config.js
+++ ngcc.config.js
@@ -5,5 +5,15 @@ module.exports = {
         /md-dynamic-forms-core\//,
       ]
     },
+    'md-dynamic-forms-clarity': {
+      ignorableDeepImportMatchers: [
+        /md-dynamic-forms-core\//,
+      ]
+    },
+    '@clr/angular': {
+      ignorableDeepImportMatchers: [
+        /@cds\/core\//,
+      ]
+    },
   },
 };
